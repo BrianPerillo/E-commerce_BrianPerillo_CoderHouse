8afe65264f4296ae4441d1eae2c406da3489b7d0
BrianPerillo <brian_perillo@hotmail.com>
2021-06-09 00:24:37 -0300

    order correción

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -6,11 +6,28 @@ import {db} from '../firebase';
 
 const Order = (props) => {
 
-
-
+    const [order, setOrder] = useState([])
+    const [trajoDatos, setTrajoDatos] = useState(false);
+    
     useEffect(() => {
 
-        console.log("ORDER ORDER ORDER" + props.order);
+        let pedidoOrder = new Promise((resolve, reject)=>{
+
+            setTimeout(()=> {
+                const order = props.order
+                resolve(order) 
+                console.log("sssss" + order[0].model);
+            },0)
+            
+            })
+            .then((order)=>{
+                setOrder(order);
+                setTrajoDatos(true);
+               
+            })
+
+        console.log("ORDER ORDER ORDER PROP" + props.order[0].model);
+        console.log("ORDER ORDER ORDER SETEADO" + order);
 
     }, [])
 
@@ -20,7 +37,23 @@ const Order = (props) => {
 
             {
                 
-                
+                    trajoDatos == false ?
+                    
+                        
+                            <div className="float-left">
+                               
+                            </div>
+                    
+
+                    :
+
+                        order.map((item) =>  {
+
+                            <p>ssss</p>
+                        })
+
+
+                // <p>{props.order[0].model}</p>
 
             }
 
